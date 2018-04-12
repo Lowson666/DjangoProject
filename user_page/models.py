8dff2335f31fe1855b8253dedf8dfa66156a0f33
Lowson666 <547805646@qq.com>
2018-04-12 15:57:47 +0800

    second commit

--- user_page/models.py
+++ user_page/models.py
@@ -3,10 +3,17 @@ from django.db import models
 
 
 class UserInfo(models.Model):
+    #用户名
     uname = models.CharField(max_length=20)
+    # 密码
     upwd = models.CharField(max_length=40)
+    #邮箱
     uemail = models.CharField(max_length=30)
+    #地址
     uaddress = models.CharField(max_length=100)
+    #收货地址
     uadses = models.CharField(max_length=20)
+    #邮编
     upostcode = models.CharField(max_length=6)
+    #电话
     uphone = models.CharField(max_length=11)
